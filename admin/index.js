c64240c13fdf1330edff1716000bf8e8e2e6846a
eric <github@ericjj.com>
2017-05-08 17:47:36 +0800

    change file name

--- admin/index.js
+++ admin/index.js
@@ -2,7 +2,7 @@ import User    from './user'
 import Article from './article'
 import Search  from './search'
 import Log     from './log'
-import Follow  from './Follow'
+import Follow  from './follow'
 
 
 export default {
