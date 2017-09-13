f39f11edb28a0476b0c00af377af3d5912b1da9d
eric <github@ericjj.com>
2017-09-13 11:12:59 +0800

    去除重复发送 email, 增加反馈邮件内容

--- api/v1/setting_v1.go
+++ api/v1/setting_v1.go
@@ -4,6 +4,7 @@ import (
 	"../../config"
 	"../../services"
 	"github.com/gin-gonic/gin"
+	"github.com/muesli/cache2go"
 	// "gopkg.in/mgo.v2/bson"
 )
 
@@ -12,14 +13,25 @@ type FeedBackParams struct {
 	Email   string `json:"email" binding:"required"`
 }
 
+var pool = cache2go.Cache("feedback")
+
 func FeedBack(c *gin.Context) {
 	conf := c.MustGet("config").(*config.Config)
+	ip := c.Request.Header.Get("X-real-ip")
+	ua := c.Request.UserAgent()
+
 	var params FeedBackParams
 	if c.BindJSON(&params) != nil {
 		c.JSON(400, gin.H{"msg": "params error"})
 		return
 	}
+	if pool.Exists(ip + ua) {
+		c.JSON(200, gin.H{"msg": "success, repeat"})
+		return
+	}
 
-	_ = services.SendEmail(conf.Email.Receiver, "[ HOLOREAD 意见反馈 ]", params.Content, "html")
+	content := "<p>" + params.Content + "<p>" + ip + "</p><p>" + ua + "</p><p>" + params.Email + "</p></p>"
+	pool.Add(ip+ua, 0, content)
+	go services.SendEmail(conf.Email.Receiver, "[ HOLOREAD 意见反馈 ]", content, "html")
 	c.JSON(200, gin.H{"msg": "success"})
 }
