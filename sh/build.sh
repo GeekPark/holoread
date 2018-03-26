e55e2bffe2aa2630083ab1c315c21601b78126f9
eric <github@ericjj.com>
2018-03-26 16:02:44 +0800

    update srcipt

--- sh/build.sh
+++ sh/build.sh
@@ -4,5 +4,7 @@ go get github.com/koding/multiconfig
 go get github.com/muesli/cache2go
 go get github.com/gin-gonic/gin
 go get gopkg.in/mgo.v2
+go get github.com/gin-contrib/sessions
+go get github.com/gorilla/websocket
 pwd
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build app.go
