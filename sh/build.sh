f4fa1a0dd6018af36bffb0723168b6b9c5b89d8f
eric <github@ericjj.com>
2018-03-26 15:31:37 +0800

    Add scripts

---
+++ sh/build.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+echo "build"
+go get github.com/koding/multiconfig
+go get github.com/muesli/cache2go
+go get github.com/gin-gonic/gin
+go get gopkg.in/mgo.v2
+cd ../
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build app.go
--- sh/deploy.sh
+++ sh/deploy.sh
@@ -1,8 +1,12 @@
 #! /bin/bash
 echo 'pack'
+cd ../
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build app.go
 tar -zcvf app.tar.gz app
 echo 'upload'
 scp app.tar.gz  holoread:/home/dev/www/holoread-go
+echo 'delete local files'
+rm app
+rm app.tar.gz
 echo 'restart'
 ssh -t holoread "pwd && cd www/holoread-go && ./restart.sh"
