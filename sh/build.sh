667766d0b63b0710c20ac92fe08bb8cf71ab6178
eric <github@ericjj.com>
2018-03-26 15:58:37 +0800

    update build.sh

--- sh/build.sh
+++ sh/build.sh
@@ -4,5 +4,5 @@ go get github.com/koding/multiconfig
 go get github.com/muesli/cache2go
 go get github.com/gin-gonic/gin
 go get gopkg.in/mgo.v2
-cd ../
+pwd
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build app.go
